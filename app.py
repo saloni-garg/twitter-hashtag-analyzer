94c6f1575f06a9a16f0022993665b9165464d67c
Amit <asagtani06@gmail.com>
2018-12-23 23:01:09 +0530

    implemented text input handler

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+# def hello():
+#     return "Hello World!"
+def main():
+	print("Hello")
+	return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def handle_data():
+    text = request.form['inputText']
+    print(text)
+    return text
 
 if __name__ == '__main__':
-    app.run()
+	app.debug = True
+	app.run()
 
