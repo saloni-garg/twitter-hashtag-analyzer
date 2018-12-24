305e2872d6945e5ed50e9189ba6d359f3837433f
Saloni Garg <salonigarg2310@gmail.com>
2018-12-24 16:40:16 +0530

    nltk, tokenized and extracted imp words
    Signed-off-by: Saloni Garg <salonigarg2310@gmail.com>

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
+
 app = Flask(__name__)
 
 
@@ -12,9 +17,19 @@ def main():
 
 @app.route('/', methods=['POST'])
 def handle_data():
-    text = request.form['inputText']
-    print(text)
-    return text
+	text = request.form['inputText']
+	text=text.lower()
+#	dd="The LNM Institute of Information Technology, is a deemed university located in Jaipur, India, on an 100-acre campus. The institute is a public-private partnership venture between the LNM foundation and the Government of Rajasthan and operates as an autonomous non-profit organization"    
+	words_all = word_tokenize(text)
+	imp_words=[]
+	string_to_print= "Important words extracted are:  "
+	stop_words = set(stopwords.words("english"))
+	for w in words_all:
+		if w not in stop_words:
+			imp_words.append(w)
+			string_to_print=string_to_print+"  |  "+w
+	print(imp_words)
+	return string_to_print
 
 if __name__ == '__main__':
 	app.debug = True
